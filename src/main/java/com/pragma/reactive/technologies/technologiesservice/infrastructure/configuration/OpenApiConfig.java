5a2d282d4e8ad4258db082d74b9aac96d9385c61
juanjimenezy <juanjiimenez8@gmail.com>
2025-03-17 23:07:08 -0500

    update

---
+++ src/main/java/com/pragma/reactive/technologies/technologiesservice/infrastructure/configuration/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.pragma.reactive.technologies.technologiesservice.infrastructure.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Technologies Service")
+                        .version("1.0")
+                        .description("Documentation of the api technology service"));
+    }
+}
--- src/main/java/com/pragma/reactive/technologies/technologiesservice/infrastructure/input/rest/controller/TechnologyController.java
+++ src/main/java/com/pragma/reactive/technologies/technologiesservice/infrastructure/input/rest/controller/TechnologyController.java
@@ -1,9 +1,10 @@
 package com.pragma.reactive.technologies.technologiesservice.infrastructure.input.rest.controller;
 
-import com.pragma.reactive.technologies.technologiesservice.application.dto.request.PageableRequestDTO;
 import com.pragma.reactive.technologies.technologiesservice.application.dto.request.TechnologyRequestDTO;
 import com.pragma.reactive.technologies.technologiesservice.application.dto.response.TechnologyResponseDTO;
 import com.pragma.reactive.technologies.technologiesservice.application.handler.ITechnologyHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,25 +15,29 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/technology")
 @RequiredArgsConstructor
+@Tag(name = "Technologies", description = "Gestion Technology")
 public class TechnologyController {
-
-
     private final ITechnologyHandler technologyHandler;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create Technologies", description = "this endpoint save technology")
     public Mono<TechnologyResponseDTO> saveTechnology(@Valid @RequestBody TechnologyRequestDTO technologyRequestDTO) {
         return technologyHandler.createTechnology(technologyRequestDTO);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Flux<TechnologyResponseDTO> getAllTechnologies(@Valid @RequestBody PageableRequestDTO pagueable) {
-        return technologyHandler.getTechnologies(pagueable.getPage(), pagueable.getSize(), pagueable.isAsc());
+    @Operation(summary = "Get all technologies", description = "this endpoint return a list technologies")
+    public Flux<TechnologyResponseDTO> getAllTechnologies(@RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "10") int size,
+                                                          @RequestParam(defaultValue = "true") boolean asc) {
+        return technologyHandler.getTechnologies(page, size, asc);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get technology by id", description = "this endpoint return technology")
     public Mono<TechnologyResponseDTO> getTechnology(@PathVariable Long id) {
         return technologyHandler.getTechnology(id);
     }
